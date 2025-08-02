7cffc1d16f26b9ad1d81b71db538ffb3dc40d65c
Boiko Artem <a.boiko@dis-group.ru>
2025-08-02 14:24:34 +0300

    Поправил замечаний с sonarlint

--- src/main/java/com/app/todo/dto/TaskDTO.java
+++ src/main/java/com/app/todo/dto/TaskDTO.java
@@ -3,26 +3,23 @@ package com.app.todo.dto;
 import com.app.todo.enums.Status;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import lombok.Data;
 
-import java.sql.Timestamp;
-
 @Data
 public class TaskDTO {
-    @NotBlank(message = "Название задачи обязательно")
-    @Size(max = 250, message = "Не более 250 символов")
-    private String name;
+  @NotBlank(message = "Название задачи обязательно")
+  @Size(max = 250, message = "Не более 250 символов")
+  private String name;
 
-    @NotBlank(message = "Пользователь обязателен")
-    private String user;
+  @NotBlank(message = "Пользователь обязателен")
+  private String user;
 
-    @Size(max = 500, message = "Описание слишком длинное (макс 500)")
-    private String description;
-    private LocalDateTime deadline;
+  @Size(max = 500, message = "Описание слишком длинное (макс 500)")
+  private String description;
+  private LocalDateTime deadline;
 
-    @NotNull(message = "Статус обязателен")
-    private Status status;
+  @NotNull(message = "Статус обязателен")
+  private Status status;
 }
